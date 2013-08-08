39c7f701125708c891703969ac9bd308cbc8d3b2
Sergey Krivohatskiy <cthutq66a@yandex.ru>
2013-08-08 11:43:26 +0700

    Some files were added.

---
+++ update-notifier-backend/test_database_create.sql
@@ -0,0 +1,39 @@
+drop database if exists update_notifier_test;
+create database if not exists update_notifier_test character set utf8;
+use update_notifier_test;
+# Index for user's mail?
+create table users (
+	id bigint(20) unsigned auto_increment, 
+	email varchar(50) not null,
+	primary key (id)
+);
+
+create table resources (
+	id bigint(20) unsigned auto_increment, 
+	user_id bigint(20) unsigned not null,
+	url varchar(255) not null,
+	shedule_code tinyint not null,
+	dom_path varchar(255) not null,
+	filter varchar(255),
+	hash int not null,
+	last_update timestamp,
+	primary key (id),
+	foreign key (user_id) references users(id) on delete cascade
+);
+
+create table tags (
+	id bigint(20) unsigned auto_increment, 
+	user_id bigint(20) unsigned not null,
+	name varchar(30) not null,
+	primary key (id),
+	constraint no_duplicate_tags unique(user_id, name),
+	foreign key (user_id) references users(id) on delete cascade
+);
+
+create table resource_tag (
+	resource_id bigint(20) unsigned, 
+	tag_id bigint(20) unsigned, 
+	primary key (tag_id, resource_id),
+	foreign key (tag_id) references tags(id),
+	foreign key (resource_id) references resources(id) on delete cascade
+);
